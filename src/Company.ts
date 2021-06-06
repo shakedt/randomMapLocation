e6a3f6b75a41835c0a84c985f3d5722b3f78a87f
shaked tayeb <dokuroc@gmail.com>
2021-06-06 15:16:28 +0300

    update mappable

--- src/Company.ts
+++ src/Company.ts
@@ -1,19 +1,28 @@
 import faker from 'faker';
 
 export class Company {
-    name: string;
-    catchPhrase: string;
-    location: {
-        lat: number,
-        lng: number
-    }
+  name: string;
+  catchPhrase: string;
+  location: {
+    lat: number;
+    lng: number;
+  };
 
-    constructor() {
-        this.name = faker.company.companyName();
-        this.catchPhrase = faker.company.catchPhrase();
-        this.location = {
-            lat: parseFloat(faker.address.latitude()),
-            lng: parseFloat(faker.address.longitude())
-        }
-    }
+  constructor() {
+    this.name = faker.company.companyName();
+    this.catchPhrase = faker.company.catchPhrase();
+    this.location = {
+      lat: parseFloat(faker.address.latitude()),
+      lng: parseFloat(faker.address.longitude())
+    };
+  }
+
+  markerContent(): string {
+    return `
+      <div>
+        <h1> Company Name: ${this.name} </h1>
+        <h3> Catchphrase: ${this.catchPhrase} </h3>
+      </div>
+    `;
+  }
 }
